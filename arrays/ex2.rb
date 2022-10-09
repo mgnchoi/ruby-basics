9d106ed38648e6db36ff57cc070040cf457bcb09
Megan Choi <meganchoi732@gmail.com>
2022-10-08 19:54:54 -0700

    arrays exercises

---
+++ arrays/ex2.rb
@@ -0,0 +1,10 @@
+# In the code below, an array containing different types of pets is assigned to pets.
+
+pets = ['cat', 'dog', 'fish', 'lizard']
+
+# Write some code that selects 'fish' and 'lizard' from the pets array - select the two items at the same time. 
+# Assign the return value to a variable named my_pets, then print the contents of my_pets as a single string, e.g.:
+
+my_pets = pets[2..3]
+
+puts my_pets.to_s
